ff860167b80f560c933820f7ac96863022fedfe9
slava_mamchur <slava_m@te.net.ua>
2018-02-21 16:55:54 +0200

    Compressed textures support implementation in progress -> save stable revision.

--- DiceGameLogic/src/main/java/com/sadgames/dicegame/logic/client/GameConst.java
+++ DiceGameLogic/src/main/java/com/sadgames/dicegame/logic/client/GameConst.java
@@ -7,10 +7,10 @@ import java.util.List;
 
 public class GameConst {
 
-    public static final String SEA_BOTTOM_TEXTURE     = "sea_bottom.jpg";
+    public static final String SEA_BOTTOM_TEXTURE     = "sea_bottom.pkm";
     public static final String NORMALMAP_TEXTURE      = "normalmap.pkm";
     public static final String DUDVMAP_TEXTURE        = "dudvmap.pkm";
-    public static final String DICE_TEXTURE           = "dice_texture.jpg";
+    public static final String DICE_TEXTURE           = "dice_texture.pkm";
 
     public static final String ROLLING_DICE_SOUND     = "rolling_dice.mp3";
 
