b58865ceee038e7fb8f5798ebbd279e5d934529a
slava_mamchur <slava_m@te.net.ua>
2018-02-23 11:49:17 +0200

    cubemap reflection on water logic implementation. Save stable revision.

--- gl3d_engine/src/main/java/com/sadgames/gl3dengine/glrender/scene/camera/FixedIsometricCamera.java
+++ gl3d_engine/src/main/java/com/sadgames/gl3dengine/glrender/scene/camera/FixedIsometricCamera.java
@@ -16,8 +16,16 @@ public class FixedIsometricCamera extends GLCamera {
         Vector3f cameraPos = new Vector3f(getCameraPosition());
         Vector3f direction = getCameraDirection();
 
-        cameraPos.y = cos(angle) * (cameraPos.y - direction.y) - sin(angle) * (cameraPos.z - direction.z) + direction.y;
-        cameraPos.z = sin(angle) * (cameraPos.y - direction.y) + cos(angle) * (cameraPos.z - direction.z) + direction.z;
+        if ((yaw > -45 && yaw <= 45) || (yaw >= 135 && yaw <= 225)) {
+            angle = (yaw > -45 && yaw <= 45) ? angle : -angle;
+            cameraPos.y = cos(angle) * (cameraPos.y - direction.y) - sin(angle) * (cameraPos.z - direction.z) + direction.y;
+            cameraPos.z = sin(angle) * (cameraPos.y - direction.y) + cos(angle) * (cameraPos.z - direction.z) + direction.z;
+        }
+        else {
+            angle = (yaw <= -45f && yaw >= -89.999f) || (yaw >= 224.999f && yaw <= 270f) ? -angle : angle;
+            cameraPos.x = cos(angle) * (cameraPos.x - direction.x) - sin(angle) * (cameraPos.y - direction.y) + direction.x;
+            cameraPos.y = sin(angle) * (cameraPos.x - direction.x) + cos(angle) * (cameraPos.y - direction.y) + direction.y;
+        }
 
         float oldPitch = pitch;
         directSetPitchByDirection(getCameraDirection(cameraPos));
