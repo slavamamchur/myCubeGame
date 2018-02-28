8f24b57303e542b0bdd03d66bfbe59ca8dd163b1
slava_mamchur <slava_m@te.net.ua>
2018-02-28 14:46:10 +0200

    Orthogonal camera for dice throwing mode implementation in progress.

--- gl3d_engine/src/main/java/com/sadgames/gl3dengine/glrender/scene/camera/Orthogonal2DCamera.java
+++ gl3d_engine/src/main/java/com/sadgames/gl3dengine/glrender/scene/camera/Orthogonal2DCamera.java
@@ -7,6 +7,7 @@ import javax.vecmath.Vector3f;
 public class Orthogonal2DCamera extends GLCamera {
 
     private float landSize;
+    private float scaleFactor = 1.0f;
 
     public Orthogonal2DCamera(float landSize) {
         super(new Vector3f(0, landSize / 2f, 0), 90, 0, 0);
@@ -17,12 +18,20 @@ public class Orthogonal2DCamera extends GLCamera {
     }
 
     @Override
+    public void setVfov(float vfov) {
+        this.vfov = vfov;
+        this.scaleFactor = vfov/ 90f;
+
+        updateProjectionMatrix();
+    }
+
+    @Override
     protected void updateProjectionMatrix() {
         float landHalfSize = landSize / 2f;
-        float left = -landHalfSize * aspectRatio;
-        float right = landHalfSize * aspectRatio;
-        float bottom = -landHalfSize;
-        float top = landHalfSize;
+        float left = -landHalfSize * aspectRatio * scaleFactor;
+        float right = landHalfSize * aspectRatio * scaleFactor;
+        float bottom = -landHalfSize * scaleFactor;
+        float top = landHalfSize * scaleFactor;
 
         MathUtils.setIdentityM(projectionMatrix, 0);
         MathUtils.orthoM(projectionMatrix, 0, left, right, bottom, top, NEAR_PLANE, FAR_PLANE);
