1e82cf6e769c245cf0a34deeff2e7d2241b1c3e9
slava_mamchur <slava_m@te.net.ua>
2018-03-04 11:13:03 +0200

    Low poly blender models creation for chip and dice in progress.

--- DiceGameLogic/src/main/java/com/sadgames/dicegame/logic/client/entities/items/GameDiceItem.java
+++ DiceGameLogic/src/main/java/com/sadgames/dicegame/logic/client/entities/items/GameDiceItem.java
@@ -4,6 +4,9 @@ import com.sadgames.gl3dengine.glrender.scene.objects.CubePrimitiveObject;
 import com.sadgames.gl3dengine.glrender.scene.shaders.GLShaderProgram;
 import com.sadgames.sysutils.common.SysUtilsWrapperInterface;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.vecmath.Vector3f;
 
 import static com.sadgames.dicegame.logic.client.GameConst.DICE_MESH_OBJECT_1;
@@ -12,7 +15,16 @@ public class GameDiceItem extends CubePrimitiveObject { //TODO: Blender3DObject:
 
     public static final float GAME_DICE_HALF_SIZE = 0.15f;
     private static final float DICE_DEFAULT_WEIGHT = 10f;
-    private static short[] DICE_FACE_VALUES = {2, 1, 5, 6, 3, 4};
+    private static Map<Integer, Integer> DICE_FACES_VALUES = new HashMap<Integer, Integer>() {
+        {
+            put(12, 1);
+            put(0,  2);
+            put(48, 3);
+            put(60, 4);
+            put(24, 5);
+            put(36, 6);
+        }
+    };
 
     public GameDiceItem(SysUtilsWrapperInterface sysUtilsWrapper, GLShaderProgram program, String textureName) {
         super(sysUtilsWrapper, textureName, program, DICE_DEFAULT_WEIGHT, MOVING_OBJECT, GAME_DICE_HALF_SIZE);
@@ -28,17 +40,18 @@ public class GameDiceItem extends CubePrimitiveObject { //TODO: Blender3DObject:
         int result = 0;
         float max_y = 0f;
 
+        int idx;
         for (int i = 0; i < 6; i++) {
-            int idx = i * 12;
+            idx = i * 12;
             Vector3f normal_vector =
                     getSysUtilsWrapper().mulMV(getModelMatrix(), new float[]{normal[idx], normal[idx + 1], normal[idx + 2], 1.0f});
 
             if (normal_vector.y > max_y) {
                 max_y = normal_vector.y;
-                result = i;
+                result = idx;
             }
         }
 
-        return DICE_FACE_VALUES[result];
+        return DICE_FACES_VALUES.get(result);
     }
 }
