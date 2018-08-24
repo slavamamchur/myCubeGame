15e88930bb02b40d81bbca4a9f8451a8de915cdb
slava mamchur <slava_m@te.net.ua>
2018-08-24 20:53:00 +0300

    Shadowmap bugfix in progress.

--- gl3d_engine/src/main/java/com/sadgames/gl3dengine/glrender/scene/objects/SpherePrimitiveObject.java
+++ gl3d_engine/src/main/java/com/sadgames/gl3dengine/glrender/scene/objects/SpherePrimitiveObject.java
@@ -13,7 +13,8 @@ public class SpherePrimitiveObject extends GameItemObject {
     protected float _mPosX;
     protected float _mPosY;
     protected float _mPosZ;
-    protected List<Float> _mVertices = new ArrayList<>();
+    protected List<Float> _mVertices;
+    protected List<Integer> _mIndices;
     protected int _mCountVertex;
     protected int _mCountIndex;
     protected int _mCountNormal;
@@ -73,117 +74,109 @@ public class SpherePrimitiveObject extends GameItemObject {
         return _mCountVertex - 1;
     }
 
+    private float len(float x, float y, float z) {
+        return (float) Math.sqrt(x*x + y*y + z*z);
+    }
+
     private void CreateIcosphere(int refineLevel) {
         //TODO:
         //Start by creating the 12 vertices of an icosahedron
         //Phi is the golden ratio
         //http://paulbourke.net/geometry/platonic/
-        /*float phi = (1.0f + sqrt(5.0f)) / 2.0f;
+        
+        float phi = (float) ((1.0f + Math.sqrt(5.0)) / 2.0f);
 
         _mCountVertex = 12;
-        _mVertices = new vector<GLfloat>;
-        _mVertices->push_back(-1); _mVertices->push_back( phi); _mVertices->push_back(0); //v0
-        _mVertices->push_back( 1); _mVertices->push_back( phi); _mVertices->push_back(0); //v1
-        _mVertices->push_back(-1); _mVertices->push_back(-phi); _mVertices->push_back(0); //v2
-        _mVertices->push_back( 1); _mVertices->push_back(-phi); _mVertices->push_back(0); //v3
+        _mVertices = new ArrayList<>();
 
-        _mVertices->push_back(0); _mVertices->push_back(-1); _mVertices->push_back( phi); //v4
-        _mVertices->push_back(0); _mVertices->push_back( 1); _mVertices->push_back( phi); //v5
-        _mVertices->push_back(0); _mVertices->push_back(-1); _mVertices->push_back(-phi); //v6
-        _mVertices->push_back(0); _mVertices->push_back( 1); _mVertices->push_back(-phi); //v7
+        float plen = len(-1f, phi, 0f);
 
-        _mVertices->push_back( phi); _mVertices->push_back(0); _mVertices->push_back(-1); //v8
-        _mVertices->push_back( phi); _mVertices->push_back(0); _mVertices->push_back( 1); //v9
-        _mVertices->push_back(-phi); _mVertices->push_back(0); _mVertices->push_back(-1); //v10
-        _mVertices->push_back(-phi); _mVertices->push_back(0); _mVertices->push_back( 1); //v11
+        _mVertices.add(-1f/plen); _mVertices.add( phi/plen); _mVertices.add(0f); //v0
+        _mVertices.add( 1f/plen); _mVertices.add( phi/plen); _mVertices.add(0f); //v1
+        _mVertices.add(-1f/plen); _mVertices.add(-phi/plen); _mVertices.add(0f); //v2
+        _mVertices.add( 1f/plen); _mVertices.add(-phi/plen); _mVertices.add(0f); //v3
 
-        //Normalize vertices to make sure they're on a unit sphere
-        for (vector<GLfloat>::size_type i = 0; i < _mVertices->size(); i += 3)
-        {
-            GLfloat x = _mVertices->at(i + 0);
-            GLfloat y = _mVertices->at(i + 1);
-            GLfloat z = _mVertices->at(i + 2);
-            float len = sqrt(x*x + y*y + z*z);
+        _mVertices.add(0f); _mVertices.add(-1f/plen); _mVertices.add( phi/plen); //v4
+        _mVertices.add(0f); _mVertices.add( 1f/plen); _mVertices.add( phi/plen); //v5
+        _mVertices.add(0f); _mVertices.add(-1f/plen); _mVertices.add(-phi/plen); //v6
+        _mVertices.add(0f); _mVertices.add( 1f/plen); _mVertices.add(-phi/plen); //v7
 
-            _mVertices->at(i+0) /= len;
-            _mVertices->at(i+1) /= len;
-            _mVertices->at(i+2) /= len;
-        }
+        _mVertices.add( phi/plen); _mVertices.add(0f); _mVertices.add(-1f/plen); //v8
+        _mVertices.add( phi/plen); _mVertices.add(0f); _mVertices.add( 1f/plen); //v9
+        _mVertices.add(-phi/plen); _mVertices.add(0f); _mVertices.add(-1f/plen); //v10
+        _mVertices.add(-phi/plen); _mVertices.add(0f); _mVertices.add( 1f/plen); //v11
 
         //then create the 20 triangles of the icosahedron (indices)
         //var faces = new List<TriangleIndices>();
-        _mIndices = new vector<GLuint>;
+        _mIndices = new ArrayList<>();
         _mCountTriangle = 20;
         _mCountIndex = 20*3;
 
         //5 faces around point 0
-        _mIndices->push_back(0); _mIndices->push_back(11); _mIndices->push_back( 5);
-        _mIndices->push_back(0); _mIndices->push_back( 5); _mIndices->push_back( 1);
-        _mIndices->push_back(0); _mIndices->push_back( 1); _mIndices->push_back( 7);
-        _mIndices->push_back(0); _mIndices->push_back( 7); _mIndices->push_back(10);
-        _mIndices->push_back(0); _mIndices->push_back(10); _mIndices->push_back(11);
+        _mIndices.add(0); _mIndices.add(11); _mIndices.add( 5);
+        _mIndices.add(0); _mIndices.add( 5); _mIndices.add( 1);
+        _mIndices.add(0); _mIndices.add( 1); _mIndices.add( 7);
+        _mIndices.add(0); _mIndices.add( 7); _mIndices.add(10);
+        _mIndices.add(0); _mIndices.add(10); _mIndices.add(11);
 
         //5 adjacent faces
-        _mIndices->push_back( 1); _mIndices->push_back( 5); _mIndices->push_back(9);
-        _mIndices->push_back( 5); _mIndices->push_back(11); _mIndices->push_back(4);
-        _mIndices->push_back(11); _mIndices->push_back(10); _mIndices->push_back(2);
-        _mIndices->push_back(10); _mIndices->push_back( 7); _mIndices->push_back(6);
-        _mIndices->push_back( 7); _mIndices->push_back( 1); _mIndices->push_back(8);
+        _mIndices.add( 1); _mIndices.add( 5); _mIndices.add(9);
+        _mIndices.add( 5); _mIndices.add(11); _mIndices.add(4);
+        _mIndices.add(11); _mIndices.add(10); _mIndices.add(2);
+        _mIndices.add(10); _mIndices.add( 7); _mIndices.add(6);
+        _mIndices.add( 7); _mIndices.add( 1); _mIndices.add(8);
 
         //5 faces around point 3
-        _mIndices->push_back(3); _mIndices->push_back(9); _mIndices->push_back(4);
-        _mIndices->push_back(3); _mIndices->push_back(4); _mIndices->push_back(2);
-        _mIndices->push_back(3); _mIndices->push_back(2); _mIndices->push_back(6);
-        _mIndices->push_back(3); _mIndices->push_back(6); _mIndices->push_back(8);
-        _mIndices->push_back(3); _mIndices->push_back(8); _mIndices->push_back(9);
+        _mIndices.add(3); _mIndices.add(9); _mIndices.add(4);
+        _mIndices.add(3); _mIndices.add(4); _mIndices.add(2);
+        _mIndices.add(3); _mIndices.add(2); _mIndices.add(6);
+        _mIndices.add(3); _mIndices.add(6); _mIndices.add(8);
+        _mIndices.add(3); _mIndices.add(8); _mIndices.add(9);
 
         //5 adjacent faces
-        _mIndices->push_back(4); _mIndices->push_back(9); _mIndices->push_back( 5);
-        _mIndices->push_back(2); _mIndices->push_back(4); _mIndices->push_back(11);
-        _mIndices->push_back(6); _mIndices->push_back(2); _mIndices->push_back(10);
-        _mIndices->push_back(8); _mIndices->push_back(6); _mIndices->push_back( 7);
-        _mIndices->push_back(9); _mIndices->push_back(8); _mIndices->push_back( 1);
+        _mIndices.add(4); _mIndices.add(9); _mIndices.add( 5);
+        _mIndices.add(2); _mIndices.add(4); _mIndices.add(11);
+        _mIndices.add(6); _mIndices.add(2); _mIndices.add(10);
+        _mIndices.add(8); _mIndices.add(6); _mIndices.add( 7);
+        _mIndices.add(9); _mIndices.add(8); _mIndices.add( 1);
 
         //Refine triangles
         int currentTriCount = _mCountTriangle;
-        for (int r = 0; r < refineLevel; r++)
-        {
-            vector<GLuint> *indicesTemp = new vector<GLuint>;
-            for (int t = 0, i = 0; t<currentTriCount; t++, i += 3)
-            {
+        for (int r = 0; r < refineLevel; r++) {
+            List<Integer> indicesTemp = new  ArrayList<>();
+            for (int t = 0, i = 0; t<currentTriCount; t++, i += 3) {
                 //replace triangle by 4 triangles
-                int a = GetMiddlePoint(_mIndices->at(i), _mIndices->at(i+1));
-                int b = GetMiddlePoint(_mIndices->at(i+1), _mIndices->at(i+2));
-                int c = GetMiddlePoint(_mIndices->at(i+2), _mIndices->at(i));
+                int a = GetMiddlePoint(_mIndices.get(i), _mIndices.get(i+1));
+                int b = GetMiddlePoint(_mIndices.get(i+1), _mIndices.get(i+2));
+                int c = GetMiddlePoint(_mIndices.get(i+2), _mIndices.get(i));
 
                 //Add the new indices
                 //T1
-                indicesTemp->push_back(_mIndices->at(i));
-                indicesTemp->push_back(a);
-                indicesTemp->push_back(c);
+                indicesTemp.add(_mIndices.get(i));
+                indicesTemp.add(a);
+                indicesTemp.add(c);
                 //T2
-                indicesTemp->push_back(_mIndices->at(i + 1));
-                indicesTemp->push_back(b);
-                indicesTemp->push_back(a);
+                indicesTemp.add(_mIndices.get(i + 1));
+                indicesTemp.add(b);
+                indicesTemp.add(a);
                 //T3
-                indicesTemp->push_back(_mIndices->at(i + 2));
-                indicesTemp->push_back(c);
-                indicesTemp->push_back(b);
+                indicesTemp.add(_mIndices.get(i + 2));
+                indicesTemp.add(c);
+                indicesTemp.add(b);
                 //T4
-                indicesTemp->push_back(a);
-                indicesTemp->push_back(b);
-                indicesTemp->push_back(c);
+                indicesTemp.add(a);
+                indicesTemp.add(b);
+                indicesTemp.add(c);
             }
 
             //replace _mIndices with indicesTemp to keep the new index array
             _mIndices = indicesTemp;
-            indicesTemp = NULL;
 
             //update triangle & indices count
             _mCountTriangle *= 4;
             _mCountIndex *= 4;
             currentTriCount = _mCountTriangle;
-        }*/
+        }
     }
 
     @Override
@@ -197,7 +190,7 @@ public class SpherePrimitiveObject extends GameItemObject {
 
     @Override
     protected float[] getNormalsArray() {
-        return new float[0];
+        return new float[0];//TODO: same as vertices with negative???
     }
 
     @Override
