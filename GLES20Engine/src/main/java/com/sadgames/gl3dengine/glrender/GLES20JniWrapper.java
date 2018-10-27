8f28f3f2b52f4c7e68204c52070d4beabdfad09b
slava mamchur <slava_m@te.net.ua>
2018-10-27 20:41:31 +0300

    porting to libGDX api -> porting ogl api to libGDX

--- GLES20Engine/src/main/java/com/sadgames/gl3dengine/glrender/GLES20JniWrapper.java
+++ GLES20Engine/src/main/java/com/sadgames/gl3dengine/glrender/GLES20JniWrapper.java
@@ -177,23 +177,10 @@ public class GLES20JniWrapper {
         return glEngine.glGetUniformLocation(program, name);
     }
 
-    public static native void glUniform3fv(
-            int location,
-            int count,
-            float[] v);
-    public static native void glUniformMatrix4fv(
-            int location,
-            int count,
-            boolean transpose,
-            float[] value);
-    public static native void glUniform1i(
-            int location,
-            int x
-    );
-    public static native void glUniform1f(
-            int location,
-            float x
-    );
+    public static native void glUniform1i(int location, int value);
+    public static native void glUniform1f(int location, float value);
+    public static native void glUniform3fv(int location, int count, float[] value);
+    public static native void glUniformMatrix4fv(int location, int count, boolean transpose, float[] value);
 
     public static native void glBindBuffer(int target, int buffer);
     public static native void glDeleteBuffers(int[] buffers);
